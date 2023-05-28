09c1a3ea2a69417f9f71338db72cc57a107fb914
ncorbitt <ncorbitt01@gmail.com>
2023-05-27 17:00:55 -0700

    Updates

---
+++ precache-manifest.abd0bf4bb3c6d84ebb8e21460534056d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "31f830dd5599b33e3c2f",
+    "url": "/static/css/main.14a4f962.chunk.css"
+  },
+  {
+    "revision": "31f830dd5599b33e3c2f",
+    "url": "/static/js/main.31f830dd.chunk.js"
+  },
+  {
+    "revision": "3c7de0a92629fb9e8557",
+    "url": "/static/js/1.3c7de0a9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ced3fda099d7404fd928b63db36c20f",
+    "url": "/static/media/RightArrow.5ced3fda.svg"
+  },
+  {
+    "revision": "12a62dd8fe3461d9628d969b5d224733",
+    "url": "/static/media/resumeForPortfolio.12a62dd8.pdf"
+  },
+  {
+    "revision": "f165df828a427674ef1df20373368a29",
+    "url": "/static/media/frontendIcon.f165df82.svg"
+  },
+  {
+    "revision": "eab1363634f591594976ba3e3c4e8de7",
+    "url": "/static/media/backendIcon.eab13636.svg"
+  },
+  {
+    "revision": "11e48a75780692e24b4465e0c3eb9bb0",
+    "url": "/static/media/cloudDatabaseIcon.11e48a75.svg"
+  },
+  {
+    "revision": "b2c4255e45ef083cba5e35eeacfc3f88",
+    "url": "/static/media/languagesIcon.b2c4255e.svg"
+  },
+  {
+    "revision": "f6437b73785ab6eae33e38f8088e3bcb",
+    "url": "/static/media/LearningIcon.f6437b73.svg"
+  },
+  {
+    "revision": "d6bab7a454a3fb27ed9f4960a64b207c",
+    "url": "/static/media/Copy.d6bab7a4.svg"
+  },
+  {
+    "revision": "62f62c8025fd7e88f0caea7235b950a1",
+    "url": "/static/media/BrunoAce-Regular.62f62c80.ttf"
+  },
+  {
+    "revision": "1e271f2a8c0c0a38428bf5a2b3dcfc32",
+    "url": "/static/media/DownArrow.1e271f2a.svg"
+  },
+  {
+    "revision": "64437f62a5f81cb09cd170b61c1ca918",
+    "url": "/static/media/HomeIcon.64437f62.svg"
+  },
+  {
+    "revision": "65e734de1c6e8cdbed9dd1d8e4161143",
+    "url": "/static/media/Sent.65e734de.svg"
+  },
+  {
+    "revision": "18d1e4664fbe5a9e066cef7be4c8a4bd",
+    "url": "/index.html"
+  }
+];
